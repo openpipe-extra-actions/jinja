1aa8d9a479eea0bbb3fa4922fb88081cb36d6970
João Pinto <lamego.pinto@gmail.com>
2019-02-12 15:58:12 +0000

    Added multiitem support

--- openpipe/plugins/transform/using/jinja.py
+++ openpipe/plugins/transform/using/jinja.py
@@ -9,11 +9,18 @@ class Plugin(PluginRuntime):
     def on_start(self, config):
         env = Environment(
             loader=FileSystemLoader("."),
-            autoescape=select_autoescape(['html', 'xml'])
+            autoescape=select_autoescape(['html', 'xml']),
+            trim_blocks=True
         )
         self.template = env.get_template(config['template'])
+        self.item_list = []
 
     def on_input(self, item):
-        if not self.config['single_item']:
-            return
-        self.put(self.template.render(item))
+        if self.config['single_item']:
+            self.put(self.template.render(item))
+        else:
+            self.item_list.append(item)
+
+    def on_complete(self):
+        if self.item_list:
+            self.put(self.template.render(items=self.item_list))
