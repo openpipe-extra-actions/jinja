5a6215ef617781f7722eb738d80d46d17e32e908
João Pinto <jpinto@ccsint.eu>
2019-03-30 14:54:10 +0000

    Added inline text templating support

--- openpipe/actions/transform/using/jinja_.py
+++ openpipe/actions/transform/using/jinja_.py
@@ -2,28 +2,39 @@
 Create content using a Jinja2 template
 """
 from openpipe.pipeline.engine import ActionRuntime
-from jinja2 import Environment, FileSystemLoader, select_autoescape
+from jinja2 import Environment, FileSystemLoader, select_autoescape, BaseLoader
 
 
 class Action(ActionRuntime):
 
     required_config = """
-        template:       # Filename for a Jinja2 template
+        template:       # Filename or the text for a Jinja2 template
         single_item:    # If set to True produces the template a single item
                         # when no more input data is available
         """
+    optional_config = """
+    template_type:  "file"  # Can be file or text, if set to file, template
+                            # is used as the filename for loading the template.
+                            # If set to "text", the 'template' config values is
+                            # used for the template text.
+    """
 
     def on_start(self, config):
-        env = Environment(
-            loader=FileSystemLoader("."),
-            autoescape=select_autoescape(['html', 'xml']),
-            trim_blocks=True
-        )
-        self.template = env.get_template(config['template'])
+        if config["template_type"] == "file":
+            env = Environment(
+                loader=FileSystemLoader("."),
+                autoescape=select_autoescape(["html", "xml"]),
+                trim_blocks=True,
+            )
+            self.template = env.get_template(config["template"])
+        else:
+            self.template = Environment(
+                loader=BaseLoader, trim_blocks=True
+            ).from_string(config["template"])
         self.item_list = []
 
     def on_input(self, item):
-        if self.config['single_item']:
+        if self.config["single_item"]:
             self.put(self.template.render(item))
         else:
             self.item_list.append(item)
